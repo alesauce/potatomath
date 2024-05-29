1e3e81c7f1b1f8388486e358cbc57bab7cbe4597
Alexander Sauceda <alexander@alexandersauceda.dev>
2024-05-28 19:23:11 -0700

    Using floating point value for price of potatoes
    The first iteration was hardcoded as an `i32` to be the easiest possible
    intro to the Rust typesystem. This change implements explicit casting to floating
    point for the price calculations because very few prices are integers.

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,5 @@
 use std::io;
 
-fn calculate_total_cost(people: i32) -> i32 {
-    // Cost of a 5 pound bag of potatoes at Wal-mart. Source:
-    // https://www.perplexity.ai/search/cost-of-a-eldJfjumS3if3xm5yIQ4GA
-    let cost_potatoes: i32 = 3;
-    let pounds_potatoes_per_person: i32 = 5;
-    let cost_per_person: i32 = cost_potatoes * pounds_potatoes_per_person;
-    people * cost_per_person
-}
-
 fn main() {
     println!("Please input the number of people you are feeding.");
     let mut people_input = String::new();
@@ -16,15 +7,24 @@ fn main() {
         .read_line(&mut people_input)
         .expect("Failed to read line");
     println!("You input {people_input} people.");
-    let num_people: i32 = match people_input.trim().parse::<i32>() {
+    let people: i32 = match people_input.trim().parse::<i32>() {
         Ok(n) => n,
         Err(e) => {
             println!("Error parsing string '{}': {}", people_input, e);
             0
         }
     };
-    let total_cost: i32 = calculate_total_cost(num_people);
+    let total_cost: f32 = calculate_total_cost(people);
     println!("Your total cost per month is: ${total_cost}. Don't forget the multivitamin!")
+}
+
+fn calculate_total_cost(people: i32) -> f32 {
+    // Cost of a 5 pound bag of potatoes at Wal-mart. Source:
+    // https://www.perplexity.ai/search/cost-of-a-eldJfjumS3if3xm5yIQ4GA
+    let cost_potatoes: f32 = 3.14;
+    let pounds_potatoes_per_person: i32 = 5;
+    let cost_per_person: f32 = cost_potatoes * (pounds_potatoes_per_person as f32);
+    (people as f32) * cost_per_person
 }
 
 #[cfg(test)]
@@ -33,6 +33,6 @@ mod tests {
 
     #[test]
     fn test_calculate_total_cost() {
-        assert_eq!(calculate_total_cost(5), 75);
+        assert_eq!(calculate_total_cost(5), 78.5);
     }
 }
